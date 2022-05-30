3ce17cc8c754614fb4ffcd0c40d5f449e040afff
Yinka Adeniyi <48151917+papiyinks@users.noreply.github.com>
2022-05-30 06:47:01 +0100

    show a single event functionality added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,34 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
-    return (
-      <div>
-        <h1>Event Detail</h1>
-      </div>
-    );
-  };
-  
-  export default EventDetailPage;
-  
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+};
+
+export default EventDetailPage;
