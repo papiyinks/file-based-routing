c7c1cb376e9d734ccd0b315ba75ff48e3a784f7c
Yinka Adeniyi <48151917+papiyinks@users.noreply.github.com>
2022-05-30 09:36:09 +0100

    layout wrapper and show all events added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,14 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 const AllEventsPage = () => {
-    return (
-      <div>
-        <h1>All Events</h1>
-      </div>
-    );
-  };
-  
-  export default AllEventsPage;
-  
+  const events = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
+};
+
+export default AllEventsPage;
