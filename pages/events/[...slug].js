9b4fee1cd7a67d7d34a224568b8b394671cf4a16
Yinka Adeniyi <48151917+papiyinks@users.noreply.github.com>
2022-05-30 11:51:04 +0100

    filtered events fucntionality added

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,10 +1,72 @@
+import { useRouter } from "next/router";
+
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filterYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filterYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
     return (
-      <div>
-        <h1>Filtered Events</h1>
-      </div>
+      <>
+        <ErrorAlert>
+          <p>Invalid Filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
     );
-  };
-  
-  export default FilteredEventsPage;
-  
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found for the chosen filter</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </>
+  );
+};
+
+export default FilteredEventsPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
-    <div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
 };
 
